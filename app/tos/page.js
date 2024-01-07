c622f394511f17a3533fca9c6b4d58c44838927e
Samme20 <samme.sundh@hotmail.se>
2024-01-07 19:50:54 +0100

    Added TOS and privacy

---
+++ app/tos/page.js
@@ -0,0 +1,141 @@
+export default function TOS() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className={`mb-3 text-5xl font-semibold`}>
+        Website Terms and Conditions of Use
+      </h1>
+      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-1 lg:text-left">
+        <ol>
+          <li>
+            <h2 className={`mt-3 text-2xl font-semibold`}>1. Terms</h2>
+            <p>
+              By accessing this Website, accessible from
+              https://psyce.radicalheights.se/, you are agreeing to be bound by
+              these Website Terms and Conditions of Use and agree that you are
+              responsible for the agreement with any applicable local laws. If
+              you disagree with any of these terms, you are prohibited from
+              accessing this site. The materials contained in this Website are
+              protected by copyright and trade mark law.
+            </p>
+          </li>
+          <li>
+            <h2 className={`mt-3 text-2xl font-semibold`}>2. Use License</h2>
+
+            <p>
+              Permission is granted to temporarily download one copy of the
+              materials on PSYCE's Website for personal, non-commercial
+              transitory viewing only. This is the grant of a license, not a
+              transfer of title, and under this license you may not:
+            </p>
+
+            <ul className={`mb-3 mt-3 ml-10`} Style="list-style-type:circle;">
+              <li>modify or copy the materials;</li>
+              <li>
+                use the materials for any commercial purpose or for any public
+                display;
+              </li>
+              <li>
+                attempt to reverse engineer any software contained on PSYCE's
+                Website;
+              </li>
+              <li>
+                remove any copyright or other proprietary notations from the
+                materials; or
+              </li>
+              <li>
+                transferring the materials to another person or "mirror" the
+                materials on any other server.
+              </li>
+            </ul>
+
+            <p>
+              This will let PSYCE to terminate upon violations of any of these
+              restrictions. Upon termination, your viewing right will also be
+              terminated and you should destroy any downloaded materials in your
+              possession whether it is printed or electronic format. These Terms
+              of Service has been created with the help of the{" "}
+              <a href="https://www.termsofservicegenerator.net">
+                Terms Of Service Generator
+              </a>
+              .
+            </p>
+          </li>
+          <li>
+            <h2 className={`mt-3 text-2xl font-semibold`}>3. Disclaimer</h2>
+
+            <p>
+              All the materials on PSYCE's Website are provided "as is". PSYCE
+              makes no warranties, may it be expressed or implied, therefore
+              negates all other warranties. Furthermore, PSYCE does not make any
+              representations concerning the accuracy or reliability of the use
+              of the materials on its Website or otherwise relating to such
+              materials or any sites linked to this Website.
+            </p>
+          </li>
+          <li>
+            <h2 className={`mt-3 text-2xl font-semibold`}>4. Limitations</h2>
+
+            <p>
+              PSYCE or its suppliers will not be hold accountable for any
+              damages that will arise with the use or inability to use the
+              materials on PSYCE's Website, even if PSYCE or an authorize
+              representative of this Website has been notified, orally or
+              written, of the possibility of such damage. Some jurisdiction does
+              not allow limitations on implied warranties or limitations of
+              liability for incidental damages, these limitations may not apply
+              to you.
+            </p>
+          </li>
+          <li>
+            <h2 className={`mt-3 text-2xl font-semibold`}>
+              5. Revisions and Errata
+            </h2>
+
+            <p>
+              The materials appearing on PSYCE's Website may include technical,
+              typographical, or photographic errors. PSYCE will not promise that
+              any of the materials in this Website are accurate, complete, or
+              current. PSYCE may change the materials contained on its Website
+              at any time without notice. PSYCE does not make any commitment to
+              update the materials.
+            </p>
+          </li>
+          <li>
+            <h2 className={`mt-3 text-2xl font-semibold`}>6. Links</h2>
+
+            <p>
+              PSYCE has not reviewed all of the sites linked to its Website and
+              is not responsible for the contents of any such linked site. The
+              presence of any link does not imply endorsement by PSYCE of the
+              site. The use of any linked website is at the user's own risk.
+            </p>
+          </li>
+          <li>
+            <h2 className={`mt-3 text-2xl font-semibold`}>
+              7. Site Terms of Use Modifications
+            </h2>
+
+            <p>
+              PSYCE may revise these Terms of Use for its Website at any time
+              without prior notice. By using this Website, you are agreeing to
+              be bound by the current version of these Terms and Conditions of
+              Use.
+            </p>
+          </li>
+          <li>
+            <h2 className={`mt-3 text-2xl font-semibold`}>8. Your Privacy</h2>
+          </li>
+          <p>Please read our Privacy Policy.</p>
+          <li>
+            <h2 className={`mt-3 text-2xl font-semibold`}>9. Governing Law</h2>
+          </li>
+        </ol>
+      </div>
+
+      <p className="mt-10">
+        Any claim related to PSYCE's Website shall be governed by the laws of se
+        without regards to its conflict of law provisions.
+      </p>
+    </main>
+  );
+}
